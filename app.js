2d64eb8ab29b20d51d53f150221c6b64f9f123c4
MedvedevDev <a95.medvedev@gmail.com>
2023-10-19 00:14:36 +0300

    404 page

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ const app = express();
 // Middleware for parsing request bodies
 app.use(bodyParser.urlencoded());
 
-// Routes
-app.use(adminRoutes);
+// Outsourced routes
+//app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(trainingRoutes);
 
 // app.use('/', (req, res, next) => {
 //     console.log('Main middleware');
 //     next();
 // })
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>Page not found</h1>`)
+})
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+// GET /admin/add-training
 router.get('/add-training', (req, res, next) => {
     console.log('Add training middleware');
-    res.send(`<form action="/training" method="POST"><input type="text" name="title"><button type="submit">Add training</button></form>`)
+    res.send(`<form action="/add-training" method="POST"><input type="text" name="title"><button type="submit">Add training</button></form>`)
 })
 
-router.post('/training', (req, res, next) => {
+// POST /admin/add-training
+router.post('/add-training', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
