a494a442164d6abdd1732d6065b70e173eb06c00
MedvedevDev <a95.medvedev@gmail.com>
2023-10-23 13:41:27 +0200

    refactor

--- models/workout.js
+++ models/workout.js
@@ -1,23 +1,30 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'workouts.json'
+);
+
+const getWorkoutsFromFile = cb => {
+    fs.readFile(p, (err, fileData) => {
+        if (err) {
+            return cb([]);
+        } else {
+            return cb(JSON.parse(fileData));
+        }
+    })
+}
+
 module.exports = class Workout {
     constructor(exerciseTitle) {
         this.exerciseTitle = exerciseTitle;
     }
 
     save() {
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'workouts.json'
-        );
-
-        fs.readFile(p, (err, fileData) => {
-            let workouts = [];
-            if (!err) {
-                workouts = JSON.parse(fileData);
-            }
+        getWorkoutsFromFile(workouts => {
+            console.log(workouts)
             workouts.push(this);
             fs.writeFile(p, JSON.stringify(workouts), err => {
                 console.log(err)
@@ -26,18 +33,6 @@ module.exports = class Workout {
     }
 
     static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'workouts.json'
-        );
-
-        fs.readFile(p, (err, fileData) => {
-            if (err) {
-                cb([]);
-            }
-
-            cb(JSON.parse(fileData));
-        })
+        getWorkoutsFromFile(cb);
     }
 }
